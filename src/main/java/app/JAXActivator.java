66baf37015d0d4668afa5064b5211dac2d9c97d5
reinoudvanzoelen <reinoud95@hotmail.com>
2019-04-11 22:19:25 +0200

    navigate from root

--- src/main/java/app/JAXActivator.java
+++ src/main/java/app/JAXActivator.java
@@ -3,7 +3,7 @@ package app;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-@ApplicationPath("api")
+@ApplicationPath("/")
 public class JAXActivator extends Application {
     // Left empty intentionally
 }
