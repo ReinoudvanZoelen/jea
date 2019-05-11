46b0d121e3a932d1a4fa4a76952983daf6da0174
reinoudvanzoelen <reinoud95@hotmail.com>
2019-05-11 13:36:17 +0200

    cleanup

--- src/main/java/endpoints/AuthenticationEndpoint.java
+++ src/main/java/endpoints/AuthenticationEndpoint.java
@@ -80,46 +80,4 @@ public class AuthenticationEndpoint {
         }
         return Response.status(Response.Status.UNAUTHORIZED).build();
     }
-
-    // @GET
-    // @Path("/google/authenticate")
-    // public Response getAuthenticateGoogle(@QueryParam("code") String code) {
-    // try {
-    // // Fetch the auth code
-    // OAuth2AccessToken authtoken =
-    // googleService.getService().getAccessToken(code);
-
-    // // Fetch the information for this user using the auth code
-    // OAuthRequest request = new OAuthRequest(Verb.GET,
-    // "https://www.googleapis.com/oauth2/v2/userinfo");
-    // googleService.getService().signRequest(authtoken, request);
-    // com.github.scribejava.core.model.Response response =
-    // googleService.getService().execute(request);
-
-    // // Map JSON string to Object
-    // ObjectNode node = new ObjectMapper().readValue(response.getBody(),
-    // ObjectNode.class);
-
-    // if (node.has("email")) {
-    // String email = node.get("email").asText();
-    // Player player = playerService.getByEmailAddress(email);
-
-    // String token = "";
-    // if (player != null) {
-    // // This google account exists in our database
-    // token = jwtService.createJwt(player);
-    // } else {
-    // // This google account does not exist in our database
-    // }
-
-    // return Response.status(Response.Status.ACCEPTED).entity(token).build();
-    // }
-
-    // return Response.status(Response.Status.UNAUTHORIZED).build();
-    // } catch (Exception e) {
-    // return
-    // Response.status(Response.Status.NOT_ACCEPTABLE).entity(e.getStackTrace()).build();
-    // }
-    // }
-
 }
